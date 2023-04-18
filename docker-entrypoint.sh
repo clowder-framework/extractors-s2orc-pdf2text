7aa96e4cccfccfbfded6645d688b0f2fbd66cf81
Minu Mathew <minumpmathews@gmail.com>
2023-04-18 14:38:55 -0500

    extractor heartbeat

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ GrobidHome="$HOME/grobid-0.6.1/"
 
 if [ $1 = "extractor" ]; then
   ./setup_run_grobid.sh
-  python3 textextractor.py
+  python3 textextractor.py --heartbeat 40
 else
   exec "$@"
 fi
