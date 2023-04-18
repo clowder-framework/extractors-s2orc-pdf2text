a839a87b5c63cda2cd1a46e4a0f9a4c364beac1b
Minu Mathew <minumpmathews@gmail.com>
2023-04-18 15:24:13 -0500

    multistage docker build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-GrobidHome="$HOME/grobid-0.6.1/"
-
 if [ $1 = "extractor" ]; then
-  ./setup_run_grobid.sh
+  #./setup_run_grobid.sh
   python3 textextractor.py --heartbeat 40
 else
   exec "$@"
