bf1a1a8e35ac06fb06ade5fa678cb8c0014a5797
Minu Mathew <minumpmathews@gmail.com>
2023-04-18 17:12:03 -0500

    run setup run grobid

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 if [ $1 = "extractor" ]; then
-  ./setup_run_grobid.sh
+  ./setup_run_grobid.sh &
   python3 textextractor.py --heartbeat 40
 else
   exec "$@"
 fi
 
+# wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
 
