cffb87a8b97a4c6d13e46e1f72c52795a1329a5f
Minu Mathew <minumpmathews@gmail.com>
2023-04-21 15:11:14 -0500

    back to extractor grobid

--- doc2txt/grobid2json/grobid/grobid_client.py
+++ doc2txt/grobid2json/grobid/grobid_client.py
@@ -22,7 +22,7 @@ require something scalable too, which is not implemented for the moment.
 '''
 
 DEFAULT_GROBID_CONFIG = {
-    "grobid_server": "localhost",
+    "grobid_server": "grobid",
     #"grobid_server": "172.17.0.0",
     "grobid_port": "8070",
     "batch_size": 1000,
